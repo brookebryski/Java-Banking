a9ced0e6ec9402c1e6f8910aba81f140ec08864f
BrookeBryski <brookeabryski@gmail.com>
2021-11-30 17:21:18 -0600

    added test stubs with fail() assertions

---
+++ src/com/company/BankAccountTest.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail();
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail();
+    }
+}
