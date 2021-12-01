6b415eb40fdd7019a43c6864fb6cff418cf48da0
BrookeBryski <brookeabryski@gmail.com>
2021-12-01 14:32:03 -0600

    tests and before method

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -5,10 +5,16 @@ public class BankAccount {
     private String lastName;
     private double balance;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType = typeOfAccount;
     }
 
     // branch = true if customer is performing transaction at branch with teller
@@ -25,6 +31,10 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
     }
 
     // more methods that use firstName, lastName, etc. here
--- src/com/company/BankAccountTest.java
+++ src/com/company/BankAccountTest.java
@@ -1,21 +1,46 @@
 package com.company;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
-
+    private BankAccount account;
+    @BeforeEach
+    public void setup(){
+        account = new BankAccount("Brooke", "Bryski", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
     @org.junit.jupiter.api.Test
     void deposit() {
-        fail();
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
     }
 
     @org.junit.jupiter.api.Test
     void withdraw() {
-        fail();
+        double balance = account.withdraw(200.00, true);
+        assertEquals(800, balance, 0);
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail();
+    void getBalance_deposit() {
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+        // I tried it the way below first, and it did still pass
+        // double balance = account.getBalance();
+        // assertEquals(1000.00, balance, 0);
     }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void isChecking_true() {
+        assertTrue(account.isChecking());
+    }
+
 }
